9b4e22d4ba02d2ef016820599baf3008de2e93f7
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2024-12-13 23:27:57 +0700

    remove commented-out main function and unnecessary include

--- SRC/main.cpp
+++ SRC/main.cpp
@@ -1,19 +1,8 @@
 #ifndef UNICODE
 #define UNICODE
 #endif
-#include "./../HEADER/Setting.hpp"
 #include "./../HEADER/ProxyServer.hpp"
 #include "./../HEADER/UI.hpp"
-
-// int main() {
-//     ProxyServer proxyServer(LOCAL_PORT);
-
-//     std::signal(SIGINT, ProxyServer::stop); 
-//     proxyServer.start();
-
-//     std::cout << "Cleaning up resources...\n";
-//     return 0;
-// }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     FreeConsole();
