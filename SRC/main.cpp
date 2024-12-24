5361ba0d2a78558ca1e925b7e90eca45ebec21c9
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2024-12-24 13:54:55 +0700

    unique app

--- SRC/main.cpp
+++ SRC/main.cpp
@@ -7,6 +7,19 @@
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     FreeConsole();
+
+    HANDLE hMutex = CreateMutex(NULL, FALSE, L"MyUniqueAppMutex");
+    if (GetLastError() == ERROR_ALREADY_EXISTS) {
+        // Mutex đã tồn tại => chương trình đã chạy
+        HWND hWnd = FindWindow(NULL, L"MyUniqueAppWindow");
+        if (hWnd) {
+            // Hiển thị cửa sổ chương trình đang chạy
+            SetForegroundWindow(hWnd);
+            ShowWindow(hWnd, SW_SHOWNORMAL);
+        }
+        return 0; // Thoát chương trình mới
+    }
+
     std::string directoryName = "./GENERATED_CER";
     if (GetFileAttributesA(directoryName.c_str()) == INVALID_FILE_ATTRIBUTES) {
         if (!CreateDirectoryA(directoryName.c_str(), NULL))
@@ -17,16 +30,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     Window.start();
 
     // Xóa thư mục và tất cả nội dung bên trong
-    // directoryName += '\0'; // Kết thúc chuỗi bằng ký tự null để sử dụng với SHFileOperation
-    // SHFILEOPSTRUCTA fileOp = {0};
-    // fileOp.wFunc = FO_DELETE; // Thao tác xóa
-    // fileOp.pFrom = directoryName.c_str(); // Tên thư mục cần xóa
-    // fileOp.fFlags = FOF_NO_UI; // Không hiển thị giao diện người dùng
+    directoryName += '\0'; // Kết thúc chuỗi bằng ký tự null để sử dụng với SHFileOperation
+    SHFILEOPSTRUCTA fileOp = {0};
+    fileOp.wFunc = FO_DELETE; // Thao tác xóa
+    fileOp.pFrom = directoryName.c_str(); // Tên thư mục cần xóa
+    fileOp.fFlags = FOF_NO_UI; // Không hiển thị giao diện người dùng
 
-    // if (SHFileOperationA(&fileOp) != 0) {
-    //     std::cerr << "Error: Failed to delete directory " << directoryName << "\n";
-    // } else {
-    //     std::cout << "Directory deleted successfully.\n";
-    // }
+    if (SHFileOperationA(&fileOp) != 0) {
+        std::cerr << "Error: Failed to delete directory " << directoryName << "\n";
+    } else {
+        std::cout << "Directory deleted successfully.\n";
+    }
     return 0;
 }
